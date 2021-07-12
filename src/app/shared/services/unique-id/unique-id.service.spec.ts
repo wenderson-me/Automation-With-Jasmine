fa7a2236bc8a588fe5c6fac71782ec38c53c90b4
Wenderson <wendrson22@gmail.com>
2021-07-12 00:23:36 -0300

    arquivos iniciais

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,14 @@
+import { UniqueIdService } from "./unique-id.service";
+
+describe ('UniqueIdService', () => {
+  it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+    const service = new UniqueIdService();
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id).toContain('app-');
+  })
+
+  it('Secund test component', () => {
+
+  })
+
+});
