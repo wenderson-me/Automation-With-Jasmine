f1f5596f2e029ec0bfdb6e1fa10cd76031819208
Wenderson <wendrson22@gmail.com>
2021-08-19 00:46:16 -0300

    testes para multiplos ids

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -5,5 +5,15 @@ describe (UniqueIdService.name, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate Ids when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+    
   })
 });
