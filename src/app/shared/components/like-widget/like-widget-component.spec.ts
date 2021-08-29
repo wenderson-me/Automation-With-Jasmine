afc0dfbca620840c8c76bb4cbcebc468ed70a457
Wenderson <wendrson22@gmail.com>
2021-08-29 03:06:46 -0300

    removendo erros do console

--- src/app/shared/components/like-widget/like-widget-component.spec.ts
+++ src/app/shared/components/like-widget/like-widget-component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
 
@@ -12,7 +13,8 @@ describe(LikeWidgetComponent.name,  () => {
     //TestBed prepara o modulo para teste
     await TestBed.configureTestingModule({
       declarations: [LikeWidgetComponent],
-      providers: [UniqueIdService]
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
