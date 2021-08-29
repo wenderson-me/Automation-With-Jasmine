8d8439299bebb12881493496954ffbcd2ff881f5
Wenderson <wendrson22@gmail.com>
2021-08-29 03:01:08 -0300

    primeiro teste

--- src/app/shared/components/like-widget/like-widget-component.spec.ts
+++ src/app/shared/components/like-widget/like-widget-component.spec.ts
@@ -1,19 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
 
 LikeWidgetComponent
 
 describe(LikeWidgetComponent.name,  () => {
-  let component : LikeWidgetComponent = null;
+  //fixture: wrapper que embrulha o componente a ser testado
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach( async () => {
     //TestBed prepara o modulo para teste
     await TestBed.configureTestingModule({
       declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(LikeWidgetComponent);
   })
 
-  it('', () => {
-    
+  it('Should create component', () => {
+      const instance = fixture.componentInstance;
+      expect(instance).toBeTruthy();
   });
 });
